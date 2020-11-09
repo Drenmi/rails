2ff548ff19102f9a9b0cb2dea9eb27f4ab7d7ab2
Ted Johansson <drenmi@gmail.com>
2020-11-09 18:20:32 +0800

    Add better #inspect method for ActiveModel instances with attributes

--- activemodel/lib/active_model/attribute_methods.rb
+++ activemodel/lib/active_model/attribute_methods.rb
@@ -497,6 +497,29 @@ module ActiveModel
       end
     end
 
+    # Returns an <tt>#inspect</tt>-like string for the value of the
+    # attribute +attr_name+. String attributes are truncated up to 50
+    # characters, Date and Time attributes are returned in the
+    # <tt>:db</tt> format. Other attributes return the value of
+    # <tt>#inspect</tt> without modification.
+    #
+    #   person = Person.create!(name: 'David Heinemeier Hansson ' * 3)
+    #
+    #   person.attribute_for_inspect(:name)
+    #   # => "\"David Heinemeier Hansson David Heinemeier Hansson ...\""
+    #
+    #   person.attribute_for_inspect(:created_at)
+    #   # => "\"2012-10-22 00:15:07\""
+    #
+    #   person.attribute_for_inspect(:tag_ids)
+    #   # => "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]"
+    def attribute_for_inspect(attr_name)
+      attr_name = attr_name.to_s
+      attr_name = self.class.attribute_aliases[attr_name] || attr_name
+      value = _read_attribute(attr_name)
+      format_for_inspect(value)
+    end
+
     private
       def attribute_method?(attr_name)
         respond_to_without_attributes?(:attributes) && attributes.include?(attr_name)
@@ -515,6 +538,16 @@ module ActiveModel
 
       def _read_attribute(attr)
         __send__(attr)
+      end
+
+      def format_for_inspect(value)
+        if value.is_a?(String) && value.length > 50
+          "#{value[0, 50]}...".inspect
+        elsif value.is_a?(Date) || value.is_a?(Time)
+          %("#{value.to_s(:inspect)}")
+        else
+          value.inspect
+        end
       end
 
       module AttrNames # :nodoc:
